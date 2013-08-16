841d8d9c290557ebc3bce64ba9eeb6ff4fc9a718
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-08-16 21:28:19 +0530

    Split on broader range, anything that is not a char or number

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -9,7 +9,7 @@ class Phrase
 
   def word_count
     word_count_hash = Hash.new(0)
-    @word.split(/[ ,]/).
+    @word.split(/\W/).
       reject { |s| s == "" }.
       each { |w| word_count_hash[w] += 1 }
     word_count_hash
