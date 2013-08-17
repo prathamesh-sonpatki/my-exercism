35c64a040d3a7d59084fa06428f4accdae327115
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-08-17 15:58:13 +0530

    Remove long-word-count and used ->, and made it on single lines

--- clojure/word-count/word_count.clj
+++ clojure/word-count/word_count.clj
@@ -1,19 +1,7 @@
-(ns phrase
-  (:use [clojure.string :as str]))
+(ns phrase (:use [clojure.string :as str]))
 
-(defn- current-count [map word] (get map word 0))
+(defn- downcased-phrase [phrase] (-> phrase str/lower-case ))
 
-(defn- prepare-count-word-hash [map word]
-  (->> (current-count map word) inc (assoc map word)))
+(defn- normalize-words [phrase] (->> (downcased-phrase phrase) (re-seq #"\w+")))
 
-(defn- downcased-phrase [phrase] (str/lower-case phrase))
-
-(defn- normalize-words [phrase]
-  (->> (downcased-phrase phrase) (re-seq #"\w+")))
-
-(defn long-word-count [phrase]
-  (->> (normalize-words phrase) (reduce prepare-count-word-hash {})))
-
-;; another implementation using frequencies
-(defn word-count [phrase]
-  (frequencies (normalize-words phrase)))
+(defn word-count [phrase] (->> phrase normalize-words frequencies ))
