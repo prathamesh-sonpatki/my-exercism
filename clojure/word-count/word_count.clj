794a54c3427fbbda7a461d555002336dbcd6981a
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-08-17 15:18:15 +0530

    Use of ->> and frequencies

--- clojure/word-count/word_count.clj
+++ clojure/word-count/word_count.clj
@@ -4,12 +4,16 @@
 (defn- current-count [map word] (get map word 0))
 
 (defn- prepare-count-word-hash [map word]
-  (assoc map word (inc (current-count map word))))
+  (->> (current-count map word) inc (assoc map word)))
 
 (defn- downcased-phrase [phrase] (str/lower-case phrase))
 
 (defn- normalize-words [phrase]
-  (re-seq #"\w+" (downcased-phrase phrase)))
+  (->> (downcased-phrase phrase) (re-seq #"\w+")))
 
+(defn long-word-count [phrase]
+  (->> (normalize-words phrase) (reduce prepare-count-word-hash {})))
+
+;; another implementation using frequencies
 (defn word-count [phrase]
-  (reduce prepare-count-word-hash {} (normalize-words phrase)))
+  (frequencies (normalize-words phrase)))
