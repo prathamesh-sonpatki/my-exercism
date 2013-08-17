b0057d1d36232771c14bf60f1c76d0687348c8da
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-08-17 23:03:35 +0530

    Indentation

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -4,12 +4,15 @@ class Phrase
   end
 
   def word_count
-    normalized_words.each.with_object(Hash.new(0)) { |w, h| h[w] += 1 }
+    normalized_words.
+      each.with_object(Hash.new(0)) do |word, count_hash|
+      count_hash[word] += 1
+    end
   end
 
   private
 
-    def normalized_words
-      @word.scan(/\w+/)
-    end
+  def normalized_words
+    @word.scan(/\w+/)
+  end
 end
