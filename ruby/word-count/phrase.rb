16e84636c4dea0a41c4f4fc118fb8eeaefbdccba
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-08-19 20:30:16 +0530

    using tap its more cleaner

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -3,9 +3,15 @@ class Phrase
     @word = word.downcase
   end
 
-  def word_count
+  def word_count_without_tap
     normalized_words.each.with_object(Hash.new(0)) do |word, counts|
       counts[word] += 1
+    end
+  end
+
+  def word_count
+    Hash.new(0).tap do |count|
+      normalized_words.each { |w| count[w] += 1 }
     end
   end
 
