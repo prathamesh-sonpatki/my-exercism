f94d3ca0572ec8224f92b855f32cce4bb7c3c104
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-08-27 23:18:02 +0530

    Make anagram simpler

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -4,30 +4,12 @@ class Anagram
   end
 
   def match(candidates)
-    @candidates = candidates
-    reject_identical_words
-    generate_array_of_hashes_that_map_word_to_its_sorted_word
-    select_hashes_where_sorted_word_matches_with_input_word
-    select_original_candidates_which_matched_to_input_word
+    candidates.
+      reject {|c| c == @word }.
+      select { |w| sort_word(w) == sort_word(@word) }
   end
 
   private
-
-  def reject_identical_words
-    @candidates.reject! { |c| c == @word}
-  end
-
-  def generate_array_of_hashes_that_map_word_to_its_sorted_word
-    @candidates.map! { |c| { c => sort_word(c) } }
-  end
-
-  def select_hashes_where_sorted_word_matches_with_input_word
-    @candidates.select! { |c| c.values[0] == sort_word(@word) }
-  end
-
-  def select_original_candidates_which_matched_to_input_word
-    @candidates.map! { |c| c.keys }.flatten
-  end
 
   def sort_word(word)
     word.chars.sort.join
