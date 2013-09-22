ba2373ba99fd030701c72134551c0151c3067264
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-09-22 20:43:57 +0530

    [clojure] Use frequencies to solve anagram

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -1,10 +1,5 @@
 (ns anagram)
 
-(declare sort-word remove-identical-words)
-
-(defn anagrams-for [word, list]
-  (filter (fn [w] (= (sort-word w) (sort-word word))) (remove-identical-words word list)))
-
-(defn- sort-word [word] (sort word))
-
-(defn- remove-identical-words [word, list] (remove (fn [w] (= w word)) list))
+(defn anagrams-for
+  [word list]
+  (filter (fn [w] (= (frequencies w) (frequencies word))) list))
