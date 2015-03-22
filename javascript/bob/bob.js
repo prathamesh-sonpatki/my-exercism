c1bdd9d1e1f96e1647e4c61731ca46f099564b6b
Prathamesh Sonpatki <csonpatki@gmail.com>
2015-03-22 22:20:27 +0530

    [Javascript] Modified solution to Bob
     - Moved all the logic into respective functions.
     - Created object for responses and based on the message type, started
       responding with particular response.

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,12 +1,30 @@
 var Bob = function () {
-    Bob.prototype.hey = function(message) {
-        if (message.trim() === '')
-            return 'Fine. Be that way!';
-        if (message.toUpperCase() === message)
-            return 'Woah, chill out!';
-        if (message.slice(-1) === '?')
-            return 'Sure.';
-        return "Whatever.";
+  var isQuite = function(message) {
+    return (message.trim() === '');
+  };
+
+  var isShouting = function(message) {
+    return (message.toUpperCase() === message);
+  };
+
+  var isAsking = function(message) {
+    return (message.slice(-1) === '?');
+  };
+
+  var responses = { quite: 'Fine. Be that way!',
+                    shouting: 'Woah, chill out!',
+                    asking: 'Sure.',
+                    default: 'Whatever.' };
+
+  Bob.prototype.hey = function(message) {
+    if (isQuite(message))
+      return responses.quite;
+    if (isShouting(message))
+      return responses.shouting;
+    if (isAsking(message))
+      return responses.asking;
+
+    return responses.default;
     };
 };
 module.exports = Bob;
